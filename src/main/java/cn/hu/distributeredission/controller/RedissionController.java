b64ca12e7df21d08245aa1afd84e81d88b6622da
googlebrain@163.com <googlebrain@163.com>
2020-08-02 22:12:12 +0800

    通过redission 实现分布式锁。

--- src/main/java/cn/hu/distributeredission/controller/RedissionController.java
+++ src/main/java/cn/hu/distributeredission/controller/RedissionController.java
@@ -1,5 +1,6 @@
 package cn.hu.distributeredission.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.redisson.Redisson;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
@@ -9,25 +10,29 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @RestController
 public class RedissionController {
 
     @RequestMapping("/redissionlock")
     public String redissionLock() {
-        System.out.println("我进入了方法......");
+        /**
+         * 使用java API的方式实现分布式锁。
+         */
+        log.info("我进入了方法......");
         Config config = new Config();
         config.useSingleServer().setAddress("redis://127.0.0.1:6379");
 
         RedissonClient redisson = Redisson.create(config);
         RLock rLock = redisson.getLock("order");
         rLock.lock(30, TimeUnit.SECONDS);
-        System.out.println("我活得了锁。。。。");
+        log.info("我活得了锁。。。。");
         try {
             Thread.sleep(10000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
-            System.out.println("我释放了锁。。。。");
+            log.info("我释放了锁。。。。");
             rLock.unlock();
         }
         return "完成";
